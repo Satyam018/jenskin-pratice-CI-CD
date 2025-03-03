f40e3a27852ced22d257d9622b9b8c0029fd4920
satyam <satyam98358@gmail.com>
2025-03-03 19:17:07 +1100

    h4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("hello2");
+        System.out.println("satyam");
         Student student=new Student("dhdh",12);
         System.out.println(student.toString());
     }
