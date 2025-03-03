d49325aac25ab1aad4f04576d97522dec1298136
satyam <satyam98358@gmail.com>
2025-03-03 13:22:47 +1100

    Adding student

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Student student=new Student("dhdh",12);
+        System.out.println(student.toString());
     }
 }
